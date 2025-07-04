0b3a7e63a01acabc97d5c799f760bb4797d80b51
caramelmelmel <melodylyq262@gmail.com>
2025-07-05 03:12:30 +0800

    day 4: completed quiz 1

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -16,6 +16,13 @@ fn main() {
     // You can optionally experiment here.
 }
 
+fn calculate_price_of_apples (quantity: i32) -> i32{
+    if quantity > 40 {
+        return 1 * quantity;
+    }
+    2 * quantity
+}
+
 // Don't change the tests!
 #[cfg(test)]
 mod tests {
